88f2fff0a599edf58ea0bb9d975a3bac9f8aaa7b
unknown <youssefmakrem01@gmail.com>
2023-06-20 12:33:28 +0100

    Updated files with new changes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import "./api/api.js"
-import Chat from './components/chat.js'
-import T from './components/tts.js'
-import YouTubePlaylist from './components/playlistdownloader.js'
+// import Chat from './components/chat.js'
+// import T from './components/tts.js'
+// import YouTubePlaylist from './components/playlistdownloader.js'
 import NoteApp from './components/noteApp/noteApp.js'
-import RandomWordGenerator from './components/randomwordgenerator.js'
-import Affirmations from './components/affirmationsSwapper.js'
-import SongGenerator from './components/SongGenerator.js'
+// import RandomWordGenerator from './components/randomwordgenerator.js'
+// import Affirmations from './components/affirmationsSwapper.js'
+// import SongGenerator from './components/SongGenerator.js'
 
-import jwt_decode from 'jwt-decode';
+// import jwt_decode from 'jwt-decode';
 import "./App.css"
 function App() {
    const  ytbapi="AIzaSyD-TaFjI0WNb32yTDj9pO7i8x78IgXL19I"
