128b708d963ebe22cc146206f6b5cf7b207829db
unknown <youssefmakrem01@gmail.com>
2023-06-21 17:39:45 +0100

    Updated files with new changes

--- back-end/Public/noteappbackend.js
+++ back-end/Public/noteappbackend.js
@@ -47,7 +47,7 @@ router.post('/login', (req, res) => {
             // Passwords match, generate a token
             const token = jwt.sign({ userId: user.user_id ,userName:user.username}, secretKey);
             res.status(200).json({ message: 'Login successful', token });
-            console.log(token)
+
           } else {
             // Passwords do not match, return error response
             res.status(401).json({ error: 'Invalid credentials' });
@@ -63,9 +63,7 @@ router.post('/login', (req, res) => {
 // Sign-up route
 router.post('/signup', (req, res) => {
   const { username, password } = req.body;
-  console.log(username, password);
 
-  // Check if the username already exists in the database
   connection.query(
     'SELECT * FROM user WHERE username = ?',
     [username],
@@ -73,14 +71,14 @@ router.post('/signup', (req, res) => {
       if (err) {
         console.error('Error executing MySQL query:', err);
         res.status(500).json({ error: 'Internal Server Error' });
-        console.log("mysql prob")
+
         return;
       }
 
       if (results.length > 0) {
         // Username already exists, return error response
         res.status(409).json({ error: 'username already exist' });
-        console.log("username already exists")
+
       } else {
         // Hash the password before storing it in the database
         bcrypt.hash(password, 10, (err, hashedPassword) => {
@@ -99,12 +97,10 @@ router.post('/signup', (req, res) => {
               if (err) {
                 console.error('Error executing MySQL query:', err);
                 res.status(500).json({ error: 'Internal Server Error' });
-                console.log("mysql prob 2")
+
                 return;
               }
 
-              // User registration successful
-              console.log("sign up okay ")
               res.status(200).json({ message: 'Sign up successful' });
             }
           );
@@ -116,9 +112,9 @@ router.post('/signup', (req, res) => {
 
 function verifyToken(req, res, next) {
   const token = req.headers.authorization;
-console.log(token)
+
   if (!token) {
-    console.log("oekf,erfeflekfnf")
+
     return res.status(401).json({ error: 'Unauthorized' });
 
   }
@@ -128,7 +124,7 @@ console.log(token)
     if (err) {
       return res.status(401).json({ error: 'Invalid token' });
     }
-  console.log(decoded.user_id)
+
     req.userId = decoded.user_id;
     next();
   });
@@ -136,7 +132,7 @@ console.log(token)
 
 router.get('/protected', verifyToken, (req, res) => {
   // Accessible only if the token is valid
-  console.log("oklf")
+
   res.status(200).json({ message: 'Protected route accessed successfully' });
 });
 
@@ -156,24 +152,16 @@ router.get('/notes/:user_id', (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
         return;
       }
-       console.log(results)
+
       // Decrypt the note content before sending it in the response
       if (results.length!==0)
      { const decryptedResults = results.map((result) => {
-   console.log("in get"+IV)
-   console.log("ojfijr")
-   console.log(result.content)
 
         let decipher = crypto.createDecipheriv(algorithm,  encryptionKey, IV);
-        console.log(decipher)
+
         let decryptedContent= decipher.update(result.content, 'hex', 'utf8');
         decryptedContent += decipher.final('utf8');
-        console.log(decryptedContent)
-        // const decipher = crypto.createDecipher(algorithm, encryptionKey);
-        // console.log(" decipher "+decipher)
-        // console.log(decipher)
-        // let decryptedContent = decipher.update(result.content, 'hex', 'utf8');
-        // decryptedContent += decipher.final('utf8');
+
         return { ...result, content: decryptedContent };
       });
 
@@ -185,17 +173,14 @@ router.get('/notes/:user_id', (req, res) => {
 router.post('/notes/:user_id', (req, res) => {
   const userId = req.params.user_id;
   const { title, content } = req.body;
-console.log("mdmklfierjgnve post")
-  // Encrypt the note content before storing it in the database
-  console.log("in post"+IV)
+
+
+
   const cipher = crypto.createCipheriv(algorithm,  encryptionKey,IV);
-  // const cipher = crypto.createCipher(algorithm, encryptionKey);
 
   let encryptedContent = cipher.update(content, 'utf8', 'hex');
   encryptedContent += cipher.final('hex');
 
-  // let encryptedContent = cipher.update(content, 'utf8', 'hex');
-  // encryptedContent += cipher.final('hex');
 
   connection.query(
     'INSERT INTO note (title, content, user_id, modified_at) VALUES (?, ?, ?, CURRENT_TIMESTAMP)',
@@ -213,10 +198,40 @@ console.log("mdmklfierjgnve post")
 });
 
 
+router.put('/notes/:note_id/:user_id', (req, res) => {
+  const noteId = req.params.note_id;
+  const userId = req.params.user_id;
+  const { title, content } = req.body;
 
+
+  const cipher = crypto.createCipheriv(algorithm,  encryptionKey,IV);
+
+  let encryptedContent = cipher.update(content, 'utf8', 'hex');
+  encryptedContent += cipher.final('hex');
+
+
+  connection.query(
+    'UPDATE note SET title = ?, content = ?, modified_at = CURRENT_TIMESTAMP WHERE note_id = ? AND user_id = ?',
+    [title, encryptedContent, noteId, userId],
+    (err, results) => {
+      if (err) {
+        console.error('Error executing MySQL query:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+
+      if (results.affectedRows === 0) {
+        // Note not found or user is not authorized
+        res.status(404).json({ error: 'Note not found or unauthorized' });
+      } else {
+        res.status(200).json({ message: 'Note updated successfully' });
+      }
+    }
+  );
+});
 
 router.delete('/notes/:note_id/:user_id', (req, res) => {
-  console.log('delete')
+
   const noteId = req.params.note_id;
   const userId = req.params.user_id;
 
@@ -240,30 +255,7 @@ router.delete('/notes/:note_id/:user_id', (req, res) => {
   );
 });
 
-router.put('/notes/:note_id/:user_id', (req, res) => {
-  const noteId = req.params.note_id;
-  const userId = req.params.user_id;
-  const { title, content } = req.body;
 
-  connection.query(
-    'UPDATE note SET title = ?, content = ?, modified_at = CURRENT_TIMESTAMP WHERE note_id = ? AND user_id = ?',
-    [title, content, noteId, userId],
-    (err, results) => {
-      if (err) {
-        console.error('Error executing MySQL query:', err);
-        res.status(500).json({ error: 'Internal Server Error' });
-        return;
-      }
-
-      if (results.affectedRows === 0) {
-        // Note not found or user is not authorized
-        res.status(404).json({ error: 'Note not found or unauthorized' });
-      } else {
-        res.status(200).json({ message: 'Note updated successfully' });
-      }
-    }
-  );
-});
 
 
 
