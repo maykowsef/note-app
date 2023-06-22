4f04d8662b1bc1f059d7a30d1dc50b83c71f2e23
unknown <youssefmakrem01@gmail.com>
2023-06-22 13:55:56 +0100

    Updated files with new changes

--- back-end/Public/noteappbackendmgdb.js
+++ back-end/Public/noteappbackendmgdb.js
@@ -57,7 +57,7 @@ router.post('/login', (req, res) => {
 router.post('/signup', (req, res) => {
   const { username, password } = req.body;
 
-  const collection = client.db(dbName).db().collection('user');
+  const collection = client.db(dbName).collection('user');
   collection.findOne({ username: username }, (err, user) => {
     if (err) {
       console.error('Error executing MongoDB query:', err);
